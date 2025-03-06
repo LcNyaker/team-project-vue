ab34f4dfd684916f00efd3dd5e8a9e3f6769d75f
DulamaA <114867084+DulamaA@users.noreply.github.com>
2025-03-06 21:33:03 +0100

    📏 uppdate(cookiebar):fixed button and h2

--- src/components/CookieBar.vue
+++ src/components/CookieBar.vue
@@ -28,8 +28,8 @@ function hideCookieBar() {
   bottom: 1.25rem;
   left: 50%;
   transform: translateX(-50%);
-  width: 90%;
-  max-width: 25rem;
+  width: 320px;
+  height: 148px;
   background-color: $midnight-sky;
   color: $mels-white;
   padding: 1rem;
@@ -48,17 +48,28 @@ function hideCookieBar() {
 
   h2 {
     font-family: $font-grand-hotel;
-    font-size: 1.375rem;
+    font-size: 1.5rem;
     margin: 0;
     color: $mels-white;
+    text-align: center;
+    font-weight: 400;
+    line-height: 1.5;
   }
 
   p {
     font-family: $font-tilt-neon;
-    font-size: 0.875rem;
-    max-width: 90%;
-    line-height: 1.2;
-    margin: 0;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: $mels-white;
+    text-align: center;
+    width: 315px;
+    height: 37px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+    max-width: 18.5rem;
   }
 
   .cookie-footer {
@@ -76,15 +87,16 @@ function hideCookieBar() {
 
   button {
     background-color: transparent;
-    border: 2px solid $neon-teal;
+    border: 4px solid $neon-teal;
     color: $neon-teal;
-    padding: 8px 16px;
+    padding: 1px 10px;
     font-size: 1rem;
-    border-radius: 8px;
+    font-weight: bold;
+    border-radius: 0.75rem;
     cursor: pointer;
 
     &:hover {
-      box-shadow: 0px 0px 15px rgba(40, 250, 239, 1);
+      box-shadow: 0px 0px 20px rgba(40, 250, 239, 1);
     }
   }
 }
