1533305b7428d7cf717bf5a6ef24a55c336be988
Anna Biehl <annabiehl@hotmail.com>
2025-03-07 14:20:05 +0100

    feature(Moviecard Container): add moviecardcarousel to the homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import ImageContainer from '../components/ImageContainer.vue';
+import MoviecardContainer from '../components/MoviecardContainer.vue';
 </script>
 
 <template>
   <main>
     <ImageContainer />
+    <MoviecardContainer />
   </main>
 </template>
 <style>
