477533f5c0a8f48874150142b6a5e2e814611c47
Yaman <yaman.ghannoum@yahoo.com>
2025-03-07 08:52:23 +0100

    feat(HeaderAndMenu): Add styling for  header and navigation menu

--- src/components/fixtures/header/HeaderAndMenu.vue
+++ src/components/fixtures/header/HeaderAndMenu.vue
@@ -56,5 +56,137 @@ function toggleMenu() {
 </template>
 
 <style lang="scss" scoped>
-@import '@/style/variables.scss';
+@import '@/style/variables.scss'; // Import global SCSS variables (colors, fonts, etc.)
+
+.header {
+  height: 70px;
+  position: sticky; // Sticky to the top
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: $mels-black;
+  border-radius: 20px;
+  border: 5px solid $aqua;
+
+  // Logo styling
+  .logo {
+    display: flex;
+    align-items: center;
+
+    img {
+      height: 60px;
+      margin-left: 1rem;
+    }
+  }
+
+  // Hamburger menu styling (for mobile)
+  .hamburger {
+    width: 45px;
+    height: 35px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    cursor: pointer;
+    margin-right: 1rem;
+
+    span {
+      height: 6px;
+      background: #fff;
+      border-radius: 2px;
+      transition: all 0.3s ease;
+    }
+  }
+
+  // Navigation menu styling
+  nav {
+    position: fixed;
+    left: 6vw;
+    top: -50vh;
+    width: 80vw;
+    height: 50vh;
+    display: flex;
+    justify-content: space-around;
+    background-color: $midnight-sky;
+    transform: translateY(120%);
+    transition: transform 0.3s ease;
+    padding: 1rem;
+    border-radius: 0 50px 0 50px;
+
+    // Close button styling within the navigation
+    .close {
+      position: absolute;
+      top: 20px;
+      right: 20px;
+      background: none;
+      border: none;
+      width: 30px;
+      height: 30px;
+      cursor: pointer;
+
+      // Each line of the close icon
+      .line {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 100%;
+        height: 4px;
+        background-color: #fff;
+        border-radius: 10px;
+        transform-origin: center;
+      }
+
+      .line:first-child {
+        transform: translate(-50%, -50%) rotate(45deg);
+      }
+
+      .line:last-child {
+        transform: translate(-50%, -50%) rotate(-45deg);
+      }
+    }
+
+    // Styling for the navigation links list
+    ul {
+      list-style: none;
+      padding: 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+
+      li {
+        a {
+          color: #fff;
+          text-decoration: none;
+          font-family: $font-grand-hotel;
+          font-size: 2rem;
+          position: relative;
+
+          // Active link styling: adds an underline in neon teal
+          &.active {
+            &::after {
+              content: '';
+              display: block;
+              height: 2px;
+              background-color: $neon-teal;
+              position: absolute;
+              bottom: -4px;
+              left: 0;
+              right: 0;
+            }
+          }
+
+          // Hover state: change color to neon teal
+          &:hover {
+            color: $neon-teal;
+          }
+        }
+      }
+    }
+
+    &.open {
+      transform: translateY(0%);
+    }
+  }
+}
 </style>
