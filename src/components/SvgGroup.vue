d8d6105dc034cd10c3c9b13c70cc8b3f4538f0fe
LcNyaker <Ludde.nyaker@gmail.com>
2025-03-07 13:06:13 +0100

    ✨  feat(components) :added SvgGroup to components
    - Made components with all svg for Film page

---
+++ src/components/SvgGroup.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import DrinkSvg from '@/assets/Drink-glow.svg';
+import DriveInSvg from '@/assets/Drive in Films.svg';
+import PopcornSvg from '@/assets/Popcorn-glow.svg';
+</script>
+
+<template>
+  <div class="main-svg-container">
+    <div class="svg-container">
+      <img :src="DrinkSvg" alt="" class="drink-icon" />
+    </div>
+    <div class="svg-container">
+      <img :src="DriveInSvg" alt="" class="drivein-icon" />
+    </div>
+    <div class="svg-container">
+      <img :src="PopcornSvg" alt="" class="popcorn-icon" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.main-svg-container {
+  display: flex;
+  height: 136px;
+  min-width: 320px;
+  padding-top: 20px;
+}
+.svg-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 33%;
+}
+.drink-icon {
+  height: 80%;
+}
+.drivein-icon {
+  height: 80%;
+}
+.popcorn-icon {
+  height: 80%;
+}
+</style>
