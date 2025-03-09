51d56ba3df93062b56fbdf1534df67fed4a8d8ec
DulamaA <114867084+DulamaA@users.noreply.github.com>
2025-03-09 20:30:47 +0100

    ✨ feat(blockquote): create the blockquotecontainer

---
+++ src/components/BlockquoteContainer.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{
+  quote: string;
+}>();
+</script>
+
+<template>
+  <div class="blockquote-container">
+    <blockquote>
+      <span class="quote-icon">❝</span>
+      <p>{{ quote }}</p>
+      <span class="quote-icon">❞</span>
+    </blockquote>
+  </div>
+</template>
