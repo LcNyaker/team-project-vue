d16dbcb8c159b3950a429e7988a4e7023511ae1e
Anna Biehl <annabiehl@hotmail.com>
2025-03-10 21:21:34 +0100

    refactor(moviecard-container): correct syntax and refactor code

--- src/components/MoviecardContainer.vue
+++ src/components/MoviecardContainer.vue
@@ -25,64 +25,68 @@
   </section>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import { ref } from 'vue';
 import rebelFilm from '@/assets/movies/rebel-without-a-cause.jpg';
 import alligatorFilm from '@/assets/movies/an-alligator-named-daisy.jpg';
 import streetCat from '@/assets/movies/a-streetcar-named-desire.jpg';
 import hotFilm from '@/assets/movies/some-like-it-hot.png';
 import psychoFilm from '@/assets/movies/psycho.jpg';
 
-export default {
-  data() {
-    return {
-      currentIndex: 0,
-      movies: [
-        {
-          title: 'Rebel without a cause',
-          image: rebelFilm,
-          genre: 'Drama',
-          duration: '1h 51m',
-          time: 'Sunday, 20:00',
-        },
-        {
-          title: 'An alligator named Daisy',
-          image: alligatorFilm,
-          genre: 'Comedy',
-          duration: '1h 28m',
-          time: 'Monday, 18:30',
-        },
-        {
-          title: 'A streetcat named Desire',
-          image: streetCat,
-          genre: 'Drama',
-          duration: '2h 2m',
-          time: 'Wednesday, 21:00',
-        },
-        {
-          title: 'Some like it hot',
-          image: hotFilm,
-          genre: 'Comedy',
-          duration: '2h 1m',
-          time: 'Friday, 19:00',
-        },
-        {
-          title: 'Psycho',
-          image: psychoFilm,
-          genre: 'Horror',
-          duration: '1h 49m',
-          time: 'Saturday, 22:00',
-        },
-      ],
-    };
+interface Movie {
+  title: string;
+  image: string;
+  genre: string;
+  duration: string;
+  time: string;
+}
+
+const movies = ref<Movie[]>([
+  {
+    title: 'Rebel without a cause',
+    image: rebelFilm,
+    genre: 'Drama',
+    duration: '1h 51m',
+    time: 'Sunday, 20:00',
   },
-  methods: {
-    prev() {
-      this.currentIndex = (this.currentIndex - 1 + this.movies.length) % this.movies.length;
-    },
-    next() {
-      this.currentIndex = (this.currentIndex + 1) % this.movies.length;
-    },
+  {
+    title: 'An alligator named Daisy',
+    image: alligatorFilm,
+    genre: 'Comedy',
+    duration: '1h 28m',
+    time: 'Monday, 18:30',
   },
+  {
+    title: 'A streetcar named Desire',
+    image: streetCat,
+    genre: 'Drama',
+    duration: '2h 2m',
+    time: 'Wednesday, 21:00',
+  },
+  {
+    title: 'Some like it hot',
+    image: hotFilm,
+    genre: 'Comedy',
+    duration: '2h 1m',
+    time: 'Friday, 19:00',
+  },
+  {
+    title: 'Psycho',
+    image: psychoFilm,
+    genre: 'Horror',
+    duration: '1h 49m',
+    time: 'Saturday, 22:00',
+  },
+]);
+
+const currentIndex = ref(0);
+
+const prev = () => {
+  currentIndex.value = (currentIndex.value - 1 + movies.value.length) % movies.value.length;
+};
+
+const next = () => {
+  currentIndex.value = (currentIndex.value + 1) % movies.value.length;
 };
 </script>
 
@@ -108,7 +112,7 @@ section {
       margin-bottom: 10px;
       margin-top: 10px;
       font-family: $font-grand-hotel;
-      font-size: 32px;
+      font-size: 2rem;
     }
 
     .carousel {
@@ -145,7 +149,7 @@ section {
       }
 
       .material-icons {
-        font-size: 80px;
+        font-size: 5rem;
       }
     }
 
@@ -165,7 +169,7 @@ section {
       background-color: $mels-black;
       color: $neon-teal;
       font-family: $font-tilt-neon;
-      font-size: 18px;
+      font-size: 1.12rem;
       align-self: center;
       transition: box-shadow 0.6s ease;
     }
