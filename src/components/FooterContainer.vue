d913ad326c0f851e96220318880218f04072f325
Sokol Hatija <hatijasokol@gmail.com>
2025-03-10 14:50:30 +0100

    feat(footer) add: media queries

--- src/components/FooterContainer.vue
+++ src/components/FooterContainer.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import MelsLogo from '@/assets/Mels-drive-in-glow.svg';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPhone, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+library.add(faPhone, faEnvelope, faMapMarkerAlt, faInstagram, faFacebook);
 </script>
 <template>
   <footer class="footer">
@@ -28,26 +32,34 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 <style scoped>
 .footer {
-  background-color: #000;
+  background-color: $mels-black;
   color: #0ff;
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-between;
   padding: 20px;
   align-items: center;
   font-family: 'Arial', sans-serif;
   box-shadow: 0 -5px 10px rgba(0, 0, 0, 0.5);
-}
-.contacts,
-.social-media {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
+  widht: 100%;
+  max-width: 1200px;
+  margin: auto;
+  text-align: left;
 }
 .svg-logo {
-  width: 150px;
-  height: 150px;
-  filter: drop-shadow(0 0 5px #0ff) drop-shadow(0 0 10px #ff00ff);
+  width: 180px;
+  max-width: 20%;
+  flex-shrink: 0;
+}
+.contacts {
+  min-width: 300px;
+  text-align: left;
+  flex: 1;
+}
+.social-media {
+  min-width: 250px;
+  text-align: right;
+  flex: 1;
 }
 .neon-text {
   font-size: 24px;
@@ -56,6 +68,11 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
     0 0 5px #0ff,
     0 0 10px #0ff,
     0 0 20px #ff00ff;
+}
+.opening-hours {
+  text-align: center;
+  margin-top: 20px;
+  font-size: 14px;
 }
 p {
   margin: 5px 0;
@@ -71,7 +88,61 @@ p {
   transform: scale(1.2);
   text-shadow:
     0 0 10px #ff00ff,
-    0 0 20px #ff00ff;
+    0 0 #ff00ff;
 }
-
+@media screen and (max-width: 768px) {
+  .footer {
+    flex-direction: column;
+    padding: 15px;
+    text-align: center;
+  }
+  .svg-logo {
+    text-align: center;
+    width: 120px;
+  }
+  .contacts {
+    text-align: left;
+  }
+  .social-media {
+    text-align: right
+  }
+  .icon {
+    font-size: 20px;
+  }
+}
+@media screen and (min-width: 769px) and (max-width: 1024px) {
+  .footer {
+    flex-direction: row;
+    justify-content: space-around;
+  }
+  .contacts {
+    text-align: left;
+  }
+  .social-media {
+    text-align: right;
+  }
+}
+@media screen and (min-width: 1025px) {
+  .footer {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 30px;
+  }
+  .svg-logo {
+    width: 180px;
+    max-width: 25%;
+    flex-shrink: 0;
+  }
+  .contacts {
+    flex: 1;
+    text-align: left;
+    min-width: 250px;
+  }
+  .social-media {
+    flex: 1;
+    text-align: right;
+    min-width: 250px;
+  }
+}
 </style>
