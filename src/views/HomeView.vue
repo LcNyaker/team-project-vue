8be65e39fa5d866e9fb22f0cbe99a8b5ef006ad9
Anna Biehl <annabiehl@hotmail.com>
2025-03-10 15:22:08 +0100

    feature(moviecard-container): add screenSize uppdate to make mobile, tablet and desktop versions work

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,44 @@
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue';
 import HeroSection from '@/components/HeroSection.vue';
 import MoviecardContainer from '../components/MoviecardContainer.vue';
+import TabletMoviecard from '@/components/TabletMoviecard.vue';
+import DesktopMoviecard from '@/components/DesktopMoviecard.vue';
+
+const screenSize = ref('desktop');
+
+const updateScreenSize = () => {
+  const width = window.innerWidth;
+  if (width < 768) {
+    screenSize.value = 'mobile';
+  } else if (width < 1440) {
+    screenSize.value = 'tablet';
+  } else {
+    screenSize.value = 'desktop';
+  }
+};
+
+onMounted(() => {
+  updateScreenSize();
+  window.addEventListener('resize', updateScreenSize);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateScreenSize);
+});
 </script>
 
 <template>
   <main>
     <HeroSection />
-    <MoviecardContainer />
+    <div>
+      <MoviecardContainer v-if="screenSize === 'mobile'" />
+      <TabletMoviecard v-else-if="screenSize === 'tablet'" />
+      <DesktopMoviecard v-else />
+    </div>
   </main>
 </template>
+
 <style>
 .home {
   color: wheat;
